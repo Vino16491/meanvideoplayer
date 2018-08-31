c83101b5367113cc5657fc1feea871317227c968
vinod g chandaliya <vinodgchandaliya@gmail.com>
2018-09-01 02:30:57 +0530

    db string added for local db connect

--- server/routes/api.ts
+++ server/routes/api.ts
@@ -4,13 +4,14 @@ const mongooseDB = require("mongoose");
 
 const db =
   "mongodb://meanvideoadm:meanvideo1@ds241012.mlab.com:41012/meanvideoplayerdb";
+const dbLocal = "mongodb://localhost:27017/meanvideoplayerdb";
 mongooseDB.Promise = global.Promise;
 mongooseDB.connect(
-  db,
+  dbLocal,
   err => {
     if (err) {
       console.log("Error! in connection " + err);
-      return err
+      return err;
     }
     console.log("DB Connected Successfully");
   }
