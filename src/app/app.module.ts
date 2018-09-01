43ce32d99b3f870d2880879ebe65f02fefc0b7d0
vinod g chandaliya <vinodgchandaliya@gmail.com>
2018-09-01 20:34:59 +0530

    created components for home and video

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { VideoCenterComponent } from './video-center/video-center.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    VideoCenterComponent
   ],
   imports: [
     BrowserModule
