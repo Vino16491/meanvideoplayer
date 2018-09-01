81169d258d5824c8f3cf4133f855502a16ac86eb
vinod g chandaliya <vinodgchandaliya@gmail.com>
2018-09-01 22:09:48 +0530

    binded video center video detail and video list components with event

--- src/app/video-center/video-center.component.ts
+++ src/app/video-center/video-center.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Video } from "../video";
 
 @Component({
-  selector: 'app-video-center',
-  templateUrl: './video-center.component.html',
-  styleUrls: ['./video-center.component.css']
+  selector: "app-video-center",
+  templateUrl: "./video-center.component.html",
+  styleUrls: ["./video-center.component.css"]
 })
 export class VideoCenterComponent implements OnInit {
+  videos: Video[] = [
+    {
+      _id: "5b8a9b6f7872448e508e5b52",
+      title: "Mean Stack First Video",
+      url: "https://youtu.be/ieO2MPxc7kY",
+      desc: "MEAN stack First App Video"
+    },
+  ];
 
-  constructor() { }
+  selectedVideo: Video;
 
-  ngOnInit() {
+  constructor() {}
+
+  ngOnInit() {}
+
+  onSelectVideo(video: any) {
+    this.selectedVideo = video;
+    console.log(this.selectedVideo);
   }
-
 }
