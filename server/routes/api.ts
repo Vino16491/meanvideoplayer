06fd343ad81ca0150f512bf0f1f27c9bc3777085
vinod g chandaliya <vinodgchandaliya@gmail.com>
2018-09-01 20:27:40 +0530

    GET, POST, PUT, DEL Api Request created

--- server/routes/api.ts
+++ server/routes/api.ts
@@ -28,4 +28,62 @@ router.get("/videos", (req, res) => {
   });
 });
 
+router.get("/videos/:id", (req, res) => {
+  console.log("Get request for a single video");
+  video.findById(req.params.id).exec((err, video) => {
+    if (err) {
+      console.log("Error retrieving video: " + err);
+    } else {
+      res.json(video);
+    }
+  });
+});
+
+router.post("/videos", (req, res) => {
+  console.log("Post a video");
+  let newVideo = new video();
+  newVideo.title = req.body.title;
+  newVideo.url = req.body.url;
+  newVideo.desc = req.body.desc;
+
+  newVideo.save((err, insertVideo) => {
+    if (err) {
+      console.log("Error saving video");
+    } else {
+      res.json(insertVideo);
+    }
+  });
+});
+
+router.put("/videos/:id", (req, res) => {
+  console.log("update a video");
+  video.findByIdAndUpdate(
+    req.params.id,
+    {
+      $set: { title: req.body.title, url: req.body.url, desc: req.body.desc }
+    },
+    {
+      new: true
+    },
+    (err, updateVideo) => {
+      if (err) {
+        res.send("Error updating video");
+      } else {
+        res.json(updateVideo);
+      }
+    }
+  );
+});
+
+router.delete("/videos/:id", (req, res) => {
+  console.log("Deleting A video");
+  video.findByIdAndRemove(req.params.id, (err, delVideos) => {
+    if (err) {
+      res.send("Err deleting video");
+    } else {
+      res.json(delVideos);
+    }
+  });
+});
+
 module.exports = router;
