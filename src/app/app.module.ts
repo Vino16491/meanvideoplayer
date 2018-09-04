f7c23c34befc9d485ba4fdd9f19d51a5d59f6e83
vinod g chandaliya <vinodgchandaliya@gmail.com>
2018-09-04 21:52:25 +0530

    created pipe for playing video

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { AppRoutingModule } from "./app-routing.module";
 import { VideoListComponent } from "./video-list/video-list.component";
 import { VideoDetailComponent } from "./video-detail/video-detail.component";
 import { HttpModule } from "@angular/http";
+import { SafePipe } from './safe.pipe';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { HttpModule } from "@angular/http";
     HomeComponent,
     VideoCenterComponent,
     VideoListComponent,
-    VideoDetailComponent
+    VideoDetailComponent,
+    SafePipe
   ],
   imports: [
     BrowserModule,
